602ec5a17b190df0d54444c5779592c91dbeacc0
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-02-15 20:42:16 +0400

    add footer

---
+++ src/pages/components/Footer.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+const Footer = () => {
+  return (
+    <Link
+      href="https://github.com/mshubitidze"
+      rel="_blank"
+      className="my-6 text-lg text-white"
+    >
+      Created By @mshubitidze
+    </Link>
+  );
+};
+
+export default Footer;
